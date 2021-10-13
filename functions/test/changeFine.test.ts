c01500150da5f6c42029584e31c4f825b4c4b12c
Steven Kellner <steven.kellner@web.de>
2021-10-13 17:27:44 +0200

    Update change fine pfunction

--- functions/test/changeFine.test.ts
+++ functions/test/changeFine.test.ts
@@ -1,13 +1,13 @@
 import {privateKey} from "../src/privateKeys";
 import {guid} from "../src/TypeDefinitions/guid";
-import {auth, callFunction, getDatabaseFines, getDatabaseReasonTemplates, getDatabaseStatisticsPropertiesWithName, signInTestUser} from "./utils";
+import {auth, callFunction, firebaseError, getDatabaseFines, getDatabaseReasonTemplates, getDatabaseStatisticsPropertiesWithName, signInTestUser} from "./utils";
 import {signOut} from "firebase/auth";
-import {assert, AssertionError, expect} from "chai";
-import {FirebaseError} from "firebase-admin";
+import {assert, expect} from "chai";
 import {ReasonTemplate} from "../src/TypeDefinitions/ReasonTemplate";
 import {Fine} from "../src/TypeDefinitions/Fine";
 import { LoggingProperties } from "../src/TypeDefinitions/LoggingProperties";
 import { ParameterContainer } from "../src/TypeDefinitions/ParameterContainer";
+import { Updatable, UpdateProperties } from "../src/TypeDefinitions/UpdateProperties";
 
 describe("ChangeFine", () => {
 
@@ -43,11 +43,10 @@ describe("ChangeFine", () => {
             });
             assert.fail("A statement above should throw an exception.");
         } catch (error) {
-            if (error instanceof AssertionError) {
-                throw error;
-            }
-            expect((error as FirebaseError).code).to.equal("functions/invalid-argument");
-            expect((error as FirebaseError).message).to.equal("Couldn't parse 'clubId'. Expected type 'string', but got undefined or null.");
+            expect(firebaseError(error)).to.be.deep.equal({
+                code: "functions/invalid-argument",
+                message: "Couldn't parse 'clubId'. Expected type 'string', but got undefined or null.",
+            });
         }
     });
 
@@ -61,11 +60,10 @@ describe("ChangeFine", () => {
             });
             assert.fail("A statement above should throw an exception.");
         } catch (error) {
-            if (error instanceof AssertionError) {
-                throw error;
-            }
-            expect((error as FirebaseError).code).to.equal("functions/invalid-argument");
-            expect((error as FirebaseError).message).to.equal("Couldn't parse 'changeType'. Expected type 'string', but got undefined or null.");
+            expect(firebaseError(error)).to.be.deep.equal({
+                code: "functions/invalid-argument",
+                message: "Couldn't parse 'changeType'. Expected type 'string', but got undefined or null.",
+            });
         }
     });
 
@@ -80,11 +78,10 @@ describe("ChangeFine", () => {
             });
             assert.fail("A statement above should throw an exception.");
         } catch (error) {
-            if (error instanceof AssertionError) {
-                throw error;
-            }
-            expect((error as FirebaseError).code).to.equal("functions/invalid-argument");
-            expect((error as FirebaseError).message).to.equal("Couldn't parse ChangeType, expected 'delete' or 'update', but got invalid instead.");
+            expect(firebaseError(error)).to.be.deep.equal({
+                code: "functions/invalid-argument",
+                message: "Couldn't parse ChangeType, expected 'delete' or 'update', but got invalid instead.",
+            });
         }
     });
 
@@ -98,11 +95,10 @@ describe("ChangeFine", () => {
             });
             assert.fail("A statement above should throw an exception.");
         } catch (error) {
-            if (error instanceof AssertionError) {
-                throw error;
-            }
-            expect((error as FirebaseError).code).to.equal("functions/invalid-argument");
-            expect((error as FirebaseError).message).to.equal("Couldn't parse 'fine'. Expected type 'object', but got undefined or null.");
+            expect(firebaseError(error)).to.be.deep.equal({
+                code: "functions/invalid-argument",
+                message: "Couldn't parse 'fine'. Expected type 'object', but got undefined or null.",
+            });
         }
     });
 
@@ -117,11 +113,10 @@ describe("ChangeFine", () => {
             });
             assert.fail("A statement above should throw an exception.");
         } catch (error) {
-            if (error instanceof AssertionError) {
-                throw error;
-            }
-            expect((error as FirebaseError).code).to.equal("functions/invalid-argument");
-            expect((error as FirebaseError).message).to.equal("Couldn't parse 'fine'. Expected type 'object', but got 'invalid' from type 'string'.");
+            expect(firebaseError(error)).to.be.deep.equal({
+                code: "functions/invalid-argument",
+                message: "Couldn't parse 'fine'. Expected type 'object', but got 'invalid' from type 'string'.",
+            });
         }
     });
 
@@ -141,7 +136,7 @@ describe("ChangeFine", () => {
             changeType: "update",
             reasonTemplate: reasonTemplate.serverObject,
             updateProperties: { // TODO: remove
-                timestamp: 123456,
+                timestamp: "2011-10-14T10:42:38+0000",
                 personId: "7BB9AB2B-8516-4847-8B5F-1A94B78EC7B7",
             },
         });
@@ -152,17 +147,17 @@ describe("ChangeFine", () => {
         expect(fetchedReason).to.deep.equal(reasonTemplate);
     }
 
-    async function setFine(withTemplate: boolean, timestamp: number) {
+    async function setFine(withTemplate: boolean, timestamp: Date) {
 
         // Set fine
         const fine = new Fine.Builder().fromValue({
             id: "637d6187-68d2-4000-9cb8-7dfc3877d5ba",
             personId: "D1852AC0-A0E2-4091-AC7E-CB2C23F708D9",
-            date: 9284765,
+            date: "2011-10-14T10:42:38+0000",
             payedState: {
                 state: "unpayed",
                 updateProperties: {
-                    timestamp: 123456,
+                    timestamp: "2011-10-14T10:42:38+0000",
                     personId: "7BB9AB2B-8516-4847-8B5F-1A94B78EC7B7",
                 },
             },
@@ -174,18 +169,16 @@ describe("ChangeFine", () => {
                 amount: 1.50,
                 importance: "high",
             },
-            updateProperties: {
-                timestamp: timestamp,
-                personId: "7BB9AB2B-8516-4847-8B5F-1A94B78EC7B7",
-            },
-        }, loggingProperties.nextIndent);
+        }, loggingProperties.nextIndent) as Fine;
+        expect(fine).to.be.instanceOf(Fine);
 
+        const updatableFine = new Updatable<Fine>(fine, new UpdateProperties(timestamp, guid.fromString("7BB9AB2B-8516-4847-8B5F-1A94B78EC7B7", loggingProperties.nextIndent)));
         await callFunction("changeFine", {
             privateKey: privateKey,
             clubLevel: "testing",
             clubId: clubId.guidString,
             changeType: "update",
-            fine: fine.serverObject,
+            fine: updatableFine.serverObject,
         });
 
         // Check fine
@@ -196,14 +189,14 @@ describe("ChangeFine", () => {
 
     it("Fine set", async () => {
         await addReasonTemplate();
-        await setFine(true, 123456);
+        await setFine(true, new Date("2011-10-15T10:42:38+0000"));
 
         // Check statistic
         const statisticsList = await getDatabaseStatisticsPropertiesWithName(clubId, "changeFine", loggingProperties.nextIndent);
         expect(statisticsList.length).to.be.equal(1);
         expect(statisticsList[0]).to.be.deep.equal({
             changedFine: {
-                date: 9284765,
+                date: "2011-10-14T10:42:38.000Z",
                 fineReason: {
                     id: "9D0681F0-2045-4A1D-ABBC-6BB289934FF9",
                     reason: "Test Reason 1",
@@ -228,8 +221,8 @@ describe("ChangeFine", () => {
 
     it("Fine update custom reason", async () => {
         await addReasonTemplate();
-        await setFine(true, 123456);
-        await setFine(false, 123457);
+        await setFine(true, new Date("2011-10-15T10:42:38+0000"));
+        await setFine(false, new Date("2011-10-16T10:42:38+0000"));
 
         // Check statistic
         let statisticsList = await getDatabaseStatisticsPropertiesWithName(clubId, "changeFine", loggingProperties.nextIndent);
@@ -239,7 +232,7 @@ describe("ChangeFine", () => {
         expect(statisticsList.length).to.be.equal(1);
         expect(statisticsList[0]).to.be.deep.equal({
             previousFine: {
-                date: 9284765,
+                date: "2011-10-14T10:42:38.000Z",
                 fineReason: {
                     id: "9D0681F0-2045-4A1D-ABBC-6BB289934FF9",
                     reason: "Test Reason 1",
@@ -260,7 +253,7 @@ describe("ChangeFine", () => {
                 },
             },
             changedFine: {
-                date: 9284765,
+                date: "2011-10-14T10:42:38.000Z",
                 fineReason: {
                     reason: "Reason",
                     amount: 1.50,
@@ -284,8 +277,8 @@ describe("ChangeFine", () => {
 
     it("Fine update template id", async () => {
         await addReasonTemplate();
-        await setFine(false, 123456);
-        await setFine(true, 123457);
+        await setFine(false, new Date("2011-10-15T10:42:38+0000"));
+        await setFine(true, new Date("2011-10-16T10:42:38+0000"));
 
         // Check statistic
         let statisticsList = await getDatabaseStatisticsPropertiesWithName(clubId, "changeFine", loggingProperties.nextIndent);
@@ -295,7 +288,7 @@ describe("ChangeFine", () => {
         expect(statisticsList.length).to.be.equal(1);
         expect(statisticsList[0]).to.be.deep.equal({
             previousFine: {
-                date: 9284765,
+                date: "2011-10-14T10:42:38.000Z",
                 fineReason: {
                     reason: "Reason",
                     amount: 1.50,
@@ -315,7 +308,7 @@ describe("ChangeFine", () => {
                 },
             },
             changedFine: {
-                date: 9284765,
+                date: "2011-10-14T10:42:38.000Z",
                 fineReason: {
                     id: "9D0681F0-2045-4A1D-ABBC-6BB289934FF9",
                     reason: "Test Reason 1",
@@ -340,43 +333,30 @@ describe("ChangeFine", () => {
 
     it("Fine delete", async () => {
         await addReasonTemplate();
-        await setFine(true, 123456);
+        await setFine(true, new Date("2011-10-15T10:42:38+0000"));
 
-        const fine = new Fine.Builder().fromValue({
-            id: "637d6187-68d2-4000-9cb8-7dfc3877d5ba",
-            personId: "D1852AC0-A0E2-4091-AC7E-CB2C23F708D9",
-            date: 9284765,
-            payedState: {
-                state: "unpayed",
-                updateProperties: {
-                    timestamp: 123456,
-                    personId: "7BB9AB2B-8516-4847-8B5F-1A94B78EC7B7",
-                },
-            },
-            number: 2,
-            fineReason: {
-                reason: "Reason",
-                amount: 1.50,
-                importance: "high",
-            },
-            updateProperties: {
-                timestamp: 123457,
-                personId: "7BB9AB2B-8516-4847-8B5F-1A94B78EC7B7",
-            },
-        }, loggingProperties.nextIndent);
-
+        const fineId = guid.fromString("637d6187-68d2-4000-9cb8-7dfc3877d5ba", loggingProperties);
         await callFunction("changeFine", {
             privateKey: privateKey,
             clubLevel: "testing",
             clubId: clubId.guidString,
             changeType: "delete",
-            fine: fine.serverObject,
+            fine: {
+                id: fineId.guidString,
+                deleted: true,
+                updateProperties: {
+                    timestamp: "2011-10-16T10:42:38+0000",
+                    personId: "7BB9AB2B-8516-4847-8B5F-1A94B78EC7B7",
+                },
+            },
         });
 
         // Check fine
         const fineList = await getDatabaseFines(clubId, loggingProperties.nextIndent);
-        const fetchedFine = fineList.find(_fine => _fine.property.id.equals(fine.id))?.property;
-        expect(fetchedFine).to.be.equal(undefined);
+        const fetchedFine = fineList.find(_fine => _fine.property.id.equals(fineId))?.property;
+        expect(fetchedFine?.serverObject).to.be.deep.equal({
+            deleted: true
+        });
 
         // Check statistic
         let statisticsList = await getDatabaseStatisticsPropertiesWithName(clubId, "changeFine", loggingProperties.nextIndent);
@@ -386,7 +366,7 @@ describe("ChangeFine", () => {
         expect(statisticsList.length).to.be.equal(1);
         expect(statisticsList[0]).to.be.deep.equal({
             previousFine: {
-                date: 9284765,
+                date: "2011-10-14T10:42:38.000Z",
                 fineReason: {
                     id: "9D0681F0-2045-4A1D-ABBC-6BB289934FF9",
                     reason: "Test Reason 1",
@@ -409,6 +389,156 @@ describe("ChangeFine", () => {
         });
     });
 
-    // TODO: update deleted fine with lower timestamp
-    // TODO: update deleted fine
+    it("Update deleted fine with lower timestamp", async () => {
+        await addReasonTemplate();
+        await setFine(true, new Date("2011-10-15T10:42:38+0000"));
+
+        // Delete fine
+        const fineId = guid.fromString("637d6187-68d2-4000-9cb8-7dfc3877d5ba", loggingProperties);
+        await callFunction("changeFine", {
+            privateKey: privateKey,
+            clubLevel: "testing",
+            clubId: clubId.guidString,
+            changeType: "delete",
+            fine: {
+                id: fineId.guidString,
+                deleted: true,
+                updateProperties: {
+                    timestamp: "2011-10-16T10:42:38+0000",
+                    personId: "7BB9AB2B-8516-4847-8B5F-1A94B78EC7B7",
+                },
+            },
+        });
+
+        // Check deleted fine
+        const fineList = await getDatabaseFines(clubId, loggingProperties.nextIndent);
+        const fetchedFine = fineList.find(_fine => _fine.property.id.equals(fineId))?.property;
+        expect(fetchedFine?.serverObject).to.be.deep.equal({
+            deleted: true
+        });
+
+        // Update deleted fine
+        const fine = new Fine.Builder().fromValue({
+            id: fineId.guidString,
+            personId: "D1852AC0-A0E2-4091-AC7E-CB2C23F708D9",
+            date: "2011-10-14T10:42:38+0000",
+            payedState: {
+                state: "settled",
+                updateProperties: {
+                    timestamp: "2011-10-14T10:42:38+0000",
+                    personId: "7BB9AB2B-8516-4847-8B5F-1A94B78EC7B7",
+                },
+            },
+            number: 2,
+            fineReason: {
+                reason: "asdf",
+                amount: 1.50,
+                importance: "medium",
+            },
+        }, loggingProperties) as Fine;
+        expect(fine).to.be.instanceOf(Fine);
+        const updatableFine = new Updatable<Fine>(fine, new UpdateProperties(new Date("2011-10-15T15:42:38+0000"), guid.fromString("7BB9AB2B-8516-4847-8B5F-1A94B78EC7B7", loggingProperties.nextIndent)));
+        try {
+            await callFunction("changeFine", {
+                privateKey: privateKey,
+                clubLevel: "testing",
+                clubId: clubId.guidString,
+                changeType: "update",
+                fine: updatableFine.serverObject
+            });
+            assert.fail("A statement above should throw an exception.");
+        } catch (error) {
+            expect(firebaseError(error)).to.be.deep.equal({
+                code: "functions/cancelled",
+                message: "Server value is newer or same old than updated value.",
+            });
+        }
+    });
+
+    it("Update deleted fine", async () => {
+        await addReasonTemplate();
+        await setFine(true, new Date("2011-10-15T10:42:38+0000"));
+
+        // Delete fine
+        const fineId = guid.fromString("637d6187-68d2-4000-9cb8-7dfc3877d5ba", loggingProperties);
+        await callFunction("changeFine", {
+            privateKey: privateKey,
+            clubLevel: "testing",
+            clubId: clubId.guidString,
+            changeType: "delete",
+            fine: {
+                id: fineId.guidString,
+                deleted: true,
+                updateProperties: {
+                    timestamp: "2011-10-16T10:42:38+0000",
+                    personId: "7BB9AB2B-8516-4847-8B5F-1A94B78EC7B7",
+                },
+            },
+        });
+
+        // Check deleted fine
+        const fineList = await getDatabaseFines(clubId, loggingProperties.nextIndent);
+        const fetchedFine = fineList.find(_fine => _fine.property.id.equals(fineId))?.property;
+        expect(fetchedFine?.serverObject).to.be.deep.equal({
+            deleted: true
+        });
+
+        // Update deleted fine
+        const fine = new Fine.Builder().fromValue({
+            id: fineId.guidString,
+            personId: "D1852AC0-A0E2-4091-AC7E-CB2C23F708D9",
+            date: "2011-10-14T10:42:38+0000",
+            payedState: {
+                state: "settled",
+                updateProperties: {
+                    timestamp: "2011-10-14T10:42:38+0000",
+                    personId: "7BB9AB2B-8516-4847-8B5F-1A94B78EC7B7",
+                },
+            },
+            number: 2,
+            fineReason: {
+                reason: "asdf",
+                amount: 1.50,
+                importance: "medium",
+            },
+        }, loggingProperties) as Fine;
+        expect(fine).to.be.instanceOf(Fine);
+        const updatableFine = new Updatable<Fine>(fine, new UpdateProperties(new Date("2011-10-16T15:42:38+0000"), guid.fromString("7BB9AB2B-8516-4847-8B5F-1A94B78EC7B7", loggingProperties.nextIndent)));
+        await callFunction("changeFine", {
+            privateKey: privateKey,
+            clubLevel: "testing",
+            clubId: clubId.guidString,
+            changeType: "update",
+            fine: updatableFine.serverObject
+        });
+
+        // Check statistics
+        let statisticsList = await getDatabaseStatisticsPropertiesWithName(clubId, "changeFine", loggingProperties.nextIndent);
+        statisticsList = statisticsList.filter(statistic => {
+            return statistic.changedFine != null && statistic.changedFine.fineReason.reason == "asdf";
+        });
+        expect(statisticsList.length).to.be.equal(1);
+        expect(statisticsList[0]).to.be.deep.equal({
+            changedFine: {
+                date: "2011-10-14T10:42:38.000Z",
+                fineReason: { 
+                    amount: 1.5,
+                    importance: "medium",
+                    reason: "asdf"
+                },
+                id: "637D6187-68D2-4000-9CB8-7DFC3877D5BA",
+                number: 2,
+                payedState: { 
+                    state: "settled"
+                },
+                person: { 
+                    id: "D1852AC0-A0E2-4091-AC7E-CB2C23F708D9", 
+                    name: {
+                        first: "John",
+                        last: "Doe"
+                    }
+                }
+            }
+        });
+    });
 });
